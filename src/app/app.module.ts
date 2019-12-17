5ec9c9670a10d55e5e66987d3215cf51573adf7b
batbrain9392 <batbrain9392@gmail.com>
2019-12-17 13:24:11 +0530

    minor refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './theme/material.module';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
-import { MaterialModule } from './theme/material.module';
 
 @NgModule({
   declarations: [
