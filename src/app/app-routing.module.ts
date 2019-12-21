b3f04efdee743baff1c1f88e4ab71248c6c022c1
Debmallya Bhattacharya <debmallyabhattacharya@gmail.com>
2019-12-21 12:38:29 +0530

    base route simplified, meta tags added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,14 +7,18 @@ import { FeatureGuard } from './feature.guard';
 import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes = [
-  { path: 'auth', component: AuthComponent, canActivate: [AuthGuard] },
+  {
+    path: '',
+    pathMatch: 'full',
+    component: AuthComponent,
+    canActivate: [AuthGuard]
+  },
   {
     path: 'chat',
     loadChildren: () => import('./chat/chat.module').then(m => m.ChatModule),
     canLoad: [FeatureGuard],
     canActivate: [FeatureGuard]
-  },
-  { path: '', pathMatch: 'full', redirectTo: '/auth' }
+  }
 ];
 
 @NgModule({
