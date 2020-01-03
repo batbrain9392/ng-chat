93b9daeb4cd87cf4246742b435b069ba5d09030a
Debmallya Bhattacharya <debmallyabhattacharya@gmail.com>
2020-01-03 21:37:16 +0530

    refactor

--- src/app/chat/chat.service.ts
+++ src/app/chat/chat.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import {
   AngularFireStorage,
-  AngularFireUploadTask
+  AngularFireUploadTask,
+  AngularFireStorageReference
 } from '@angular/fire/storage';
 import { HttpClient } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -68,51 +69,52 @@ export class ChatService {
     this.uploadTask = this.storage.upload(filePath, img);
     const uploadSnapshot$ = this.uploadTask.snapshotChanges();
     uploadSnapshot$
+      .pipe(finalize(() => this.getImageUrlAndAddToChat(fileRef)))
+      .subscribe();
+  }
+
+  private getImageUrlAndAddToChat(fileRef: AngularFireStorageReference) {
+    fileRef
+      .getDownloadURL()
       .pipe(
-        finalize(() => {
-          fileRef
-            .getDownloadURL()
-            .pipe(
-              take(1),
-              mergeMap((imgUrl: string) =>
-                this.httpClient
-                  .get(
-                    `https://ff713e1f.ngrok.io/predict?image=${btoa(imgUrl)}`
-                  )
-                  .pipe(
-                    take(1),
-                    mergeMap(({ predictions }: any) => {
-                      console.table(predictions);
-                      const text =
-                        predictions &&
-                        predictions.length &&
-                        predictions[0].caption;
-                      return this.formChat().pipe(
-                        map(chat =>
-                          this.chatCollection.add({
-                            ...chat,
-                            imgUrl,
-                            text
-                          } as Chat)
-                        )
-                      );
-                    }),
-                    catchError(err => {
-                      console.error(err);
-                      this.storage.storage.refFromURL(imgUrl).delete();
-                      this.matSnackBar.open(
-                        'Server not reachable. Please try again after some time.',
-                        'CLOSE'
-                      );
-                      return of(null);
-                    }),
-                    finalize(() => this.isUploadActive.next(false))
-                  )
-              )
-            )
-            .subscribe();
-        })
+        take(1),
+        mergeMap((imgUrl: string) => this.predictCaptionAndAddToChat(imgUrl))
       )
       .subscribe();
+  }
+
+  private predictCaptionAndAddToChat(imgUrl: string) {
+    return this.httpClient
+      .get(`https://ff713e1f.ngrok.io/predict?image=${btoa(imgUrl)}`)
+      .pipe(
+        take(1),
+        mergeMap(({ predictions }: any) => this.addToChat(predictions, imgUrl)),
+        catchError(err => this.errHandler(err, imgUrl)),
+        finalize(() => this.isUploadActive.next(false))
+      );
+  }
+
+  private addToChat(predictions: any[], imgUrl: string) {
+    console.table(predictions);
+    const text = predictions[0].caption;
+    return this.formChat().pipe(
+      map(chat =>
+        this.chatCollection.add({
+          ...chat,
+          imgUrl,
+          text
+        } as Chat)
+      )
+    );
+  }
+
+  private errHandler(err: any, imgUrl: string) {
+    console.error(err);
+    this.storage.storage.refFromURL(imgUrl).delete();
+    this.matSnackBar.open(
+      'Server not reachable. Please try again after some time.',
+      'CLOSE'
+    );
+    return of(null);
   }
 }
