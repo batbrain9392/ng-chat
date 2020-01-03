dd825c662cafbc6a8d5d87df8c249730699b5ef8
Debmallya Bhattacharya <debmallyabhattacharya@gmail.com>
2020-01-03 20:00:38 +0530

    integrated ML (image captioning) always chooses the first prediction, but lists all in console

--- src/app/chat/chat.service.ts
+++ src/app/chat/chat.service.ts
@@ -4,9 +4,10 @@ import {
   AngularFireStorage,
   AngularFireUploadTask
 } from '@angular/fire/storage';
-import { UploadTaskSnapshot } from '@angular/fire/storage/interfaces';
-import { Observable, BehaviorSubject } from 'rxjs';
-import { finalize, map, mergeMap, take } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { BehaviorSubject, of } from 'rxjs';
+import { finalize, map, mergeMap, take, catchError } from 'rxjs/operators';
 import { Chat } from './chat';
 import { AuthService } from '../auth/auth.service';
 
@@ -17,13 +18,14 @@ export class ChatService {
   private uploadTask: AngularFireUploadTask;
   private isUploadActive = new BehaviorSubject<boolean>(false);
   readonly isUploadActive$ = this.isUploadActive.asObservable();
-  // uploadPercentage$: Observable<number>;
   chatCollection = this.db.collection<Chat>('chat');
 
   constructor(
     private db: AngularFirestore,
     private storage: AngularFireStorage,
-    private authService: AuthService
+    private authService: AuthService,
+    private httpClient: HttpClient,
+    private matSnackBar: MatSnackBar
   ) {}
 
   getMessages() {
@@ -65,14 +67,6 @@ export class ChatService {
     const fileRef = this.storage.ref(filePath);
     this.uploadTask = this.storage.upload(filePath, img);
     const uploadSnapshot$ = this.uploadTask.snapshotChanges();
-    // this.uploadPercentage$ = this.uploadTask.percentageChanges();
-    // this.isUploadActive$ = uploadSnapshot$.pipe(
-    //   map(
-    //     (uploadSnapshot: UploadTaskSnapshot) =>
-    //       uploadSnapshot.state === 'running' &&
-    //       uploadSnapshot.bytesTransferred < uploadSnapshot.totalBytes
-    //   )
-    // );
     uploadSnapshot$
       .pipe(
         finalize(() => {
@@ -81,12 +75,34 @@ export class ChatService {
             .pipe(
               take(1),
               mergeMap((imgUrl: string) =>
-                this.formChat().pipe(
-                  map(chat =>
-                    this.chatCollection
-                      .add({ ...chat, imgUrl } as Chat)
-                      .then(_ => this.isUploadActive.next(false))
-                  )
+                this.httpClient.get(`/predict?image=${btoa(imgUrl)}`).pipe(
+                  take(1),
+                  mergeMap(({ predictions }: any) => {
+                    console.table(predictions);
+                    const text =
+                      predictions &&
+                      predictions.length &&
+                      predictions[0].caption;
+                    return this.formChat().pipe(
+                      map(chat =>
+                        this.chatCollection.add({
+                          ...chat,
+                          imgUrl,
+                          text
+                        } as Chat)
+                      )
+                    );
+                  }),
+                  catchError(err => {
+                    console.error(err);
+                    this.storage.storage.refFromURL(imgUrl).delete();
+                    this.matSnackBar.open(
+                      'Server not reachable. Please try again after some time.',
+                      'CLOSE'
+                    );
+                    return of(null);
+                  }),
+                  finalize(() => this.isUploadActive.next(false))
                 )
               )
             )
